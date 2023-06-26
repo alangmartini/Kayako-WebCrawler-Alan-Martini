bdcf40e8377f0b15010ee89021d40963c344d80e
gmaralan <gmartinialan@gmail.com>
2023-06-26 17:36:17 -0400

    feat: created url validator applying SRP and Chain of Responsability

---
+++ backend/src/services/urlValidating/validators/domain-validator.validator.ts
@@ -0,0 +1,24 @@
+import { URL } from 'url'
+import LinkValidator from '../link-validator.validator.js'
+
+export class DomainValidator extends LinkValidator {
+  private readonly domain: string
+
+  constructor (domain: string) {
+    super()
+    this.domain = domain
+  }
+
+  validate (url: string): boolean {
+    const hostname = new URL(url).hostname
+    if (hostname !== this.domain) {
+      return false
+    }
+
+    if (this.next != null) {
+      return this.next.validate(url)
+    }
+
+    return true
+  }
+}
